9e9f02b3248024613775ea00bf644bcf037110fb
Argent <jrargent42@gmail.com>
2022-08-21 19:30:21 -0400

    add save functionality. Bug exists

--- assets/js/script.js
+++ assets/js/script.js
@@ -242,7 +242,72 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+
+
+// Gets task items from localStorage.
+
+// Converts tasks from the string format back into an array of objects.
+
+// Iterates through a tasks array and creates task elements on the page from it.
+
+
+var loadTasks = function() {
+    tasks = localStorage.getItem("tasks", tasks);
+    
+
+    if (tasks === null) {
+        
+       tasks = [];
+        return false;
+    }
+
+    tasks = JSON.parse(tasks);
+    
+    for (var i = 0; i < tasks.length; i++) {
+       
+        //pull task Id from array which has been parsed from local storage
+       taskId = tasks[i].id;
+    
+
+       var listItemEl = document.createElement("li");
+       listItemEl.className = "task-item";
+       listItemEl.setAttribute("data-task-id", tasks[i].id);
+       
+
+       var taskInfoEl = document.createElement("div")
+       taskInfoEl.className = "task-info";
+       taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+
+       listItemEl.appendChild(taskInfoEl);
+
+       var taskActionsEl = createTaskActions(tasks[i].id);
+       listItemEl.appendChild(taskActionsEl);
+       
+
+        if (tasks[i].status === "to do") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+            tasksToDoEl.appendChild(listItemEl);
+        }
+        else if (tasks[i].status === "in progress") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        }
+        else if (tasks[i].status === "complete") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            tasksCompletedEl.appendChild(listItemEl);
+        }
+        taskIdCounter++;
+        console.log(listItemEl);
+    }
+
+    
+    };
+    
+
+
+
 pageContentEl.addEventListener("click", taskButtonHandler);
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 
+loadTasks();
